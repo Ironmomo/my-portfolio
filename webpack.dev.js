eb7f98dff456ddb4acc79801c9a8a4b997d9475c
Andrin Fassbind <andrin.fassbind@gmail.com>
2025-01-09 21:07:12 +0100

    setup webpack dev environment
    -- babel
    -- devServer
    -- source map
    -- HtmlWebpackPlugin
    -- hot reloading

---
+++ webpack.dev.js
@@ -0,0 +1,52 @@
+import { fileURLToPath } from 'url'
+import path from 'path'
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+export default {
+    mode: 'development',
+    entry: {
+        bundle: ['./src/index.js', './src/template.html']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].[contenthash].js',
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        port: 4200,
+        open: true,
+        hot: true,
+        compress: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/template.html'
+        })
+    ]
+}
